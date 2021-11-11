25d4070a70d3af8470df593bf8c81be20cea1c4d
IT Common <it-21@Globegroup.com>
2021-11-11 20:13:53 +0600

    server install

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require("cors");
+const { MongoClient } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -7,12 +8,17 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
+
+// mongoDB connection
+const uri = "mongodb+srv://<username>:<password>@cluster0.wyglv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 // root api
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('BabyShoper Server is ON!!')
 })
 
 // port listener
 app.listen(port, () => {
-    console.log(`listening at:${port}`)
+    console.log(`listening at:${port}`);
 })
